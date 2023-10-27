c3f170e92536f10771845a160b5d7af0cf3d00e4
devHksingh <hksingh.dev@gmail.com>
2023-10-27 21:31:01 +0530

    add 2nd question

--- 01_Array/02_question.js
+++ 01_Array/02_question.js
@@ -7,8 +7,8 @@
  4. Using Array concat() Method
 */ 
 
-const array = [1,2,3,4]
-const firstElement = 141
+let array = [1,2,3,4]
+const firstElement = 161
 
 // 1. Using Array unshift() method 
 
@@ -23,8 +23,26 @@ function method_01(arr) {
 function method_02() {
     // console.log();
     // const test = [firstElement,array.splice(0,0,44)]
-    array.splice(0,1,6)
+//    const  firstElement= array.splice(0,1)
+    // console.log(firstElement);
+    // const lastElement =array.splice(array.length-1,1)
+    // console.log(lastElement);
+    array.splice(0,0,141);
     console.log(array);
 }
 
-method_02()
+// method_02()
+
+//  3. Using Spread operator
+function method_03() {
+    array = [firstElement,...array]
+    console.log(array);
+}
+// method_03()
+
+// 4. Using Array concat() Method
+function method_04() {
+    array = [firstElement].concat(array)
+    console.log(array);
+}
+method_04()
