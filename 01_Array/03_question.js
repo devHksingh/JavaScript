443b9f63e13570f13e89d9c89c4569857dcef62e
devHksingh <hksingh.dev@gmail.com>
2023-10-31 21:42:12 +0530

    remove element

--- 01_Array/03_question.js
+++ 01_Array/03_question.js
@@ -46,3 +46,68 @@ Method 3: Using the splice() method
 This method is used to modify the contents of an array by removing the existing elements and/or adding new elements. 
 To remove elements by the splice() method you can specify the elements in different ways.
 */
+
+const method_03 =()=>{
+    const removedElement = arr.splice(1,1)
+    console.log(`removed 1 element at index 1 and removed element in array is  `,removedElement);
+    console.log(`remaing element is `,arr);
+    // removedElement.splice(2,1,"trumpet")
+    // console.log(`Remove 1 element at index 2, and insert "trumpet" `,removedElement);
+}
+// method_03()
+// console.log(arr);
+
+// Removing the specified element by value from the array using splice method
+
+const spliceMethod=(element)=>{
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]===element) {
+            
+            console.log(`Removed element is `,arr.splice(i,1));
+            console.log(`Remaing element is `,arr);
+        }
+        
+        
+    }
+}
+
+// spliceMethod("filter")
+
+/*
+Method 4: Using the filter() method
+This method is used to create a new array from a given array consisting of only those 
+elements from the given array which satisfy a condition set by the argument function.
+ To remove elements by filter() method you can specify the elements in different ways. 
+*/
+
+
+const method_05=(element)=>{
+    if(element!=="pop"){
+        return element
+    }
+}
+// const newArr = arr.filter(method_05)
+console.log(arr);
+// console.log(newArr);
+
+// Method 5: Using Remove Method
+/*
+Creating a remove method using the filter
+ method to remove elements from a JavaScript array.
+  This method works in reverse order. 
+*/
+
+const removeElement = (arr,element)=>{
+    return arr.filter((value)=>{
+        if(value !== element){
+            return value
+        }
+    })
+}
+
+console.log(removeElement(arr,'splice'));
+
+/*
+Method 6: Using Delete Operator
+Use the delete operator to remove elements from a JavaScript array.
+*/
